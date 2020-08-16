2d2c83a0dd27d38fef9e7f4b850bd8301806fad7
donelleharris <donelle.harris@gmail.com>
2020-08-16 15:10:51 -0500

    add tests and ability to say 'hello' to any input

--- js/code.js
+++ js/code.js
@@ -3,11 +3,5 @@ const helloWorld = function () {
     return "Hello, World!";
 };
 function sayHello(input){
-    if(input === "Alex"){
-        return "Hello, Alex!"
-    } else if(input === "Pat") {
-        return "Hello, Pat!"
-    } {
-        return "Hello, Jane!";
-    }
+    return "Hello, " + input + "!";
 };
